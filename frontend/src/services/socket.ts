203987f82d5f6fe553224b54a1f8e02e611c8e9c
Lê Thành Phú <lephu240905@gmail.com>
2025-11-08 08:01:35 +0700

    fix

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,7 +1,15 @@
-// src/services/socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5001", {
+// Lấy userId từ localStorage hoặc cookie
+const userId = localStorage.getItem("userId");
+
+const BASE_URL =
+  import.meta.env.MODE === "production"
+    ? window.location.origin
+    : "http://localhost:5001";
+
+export const socket = io(BASE_URL, {
   withCredentials: true,
   transports: ["websocket"],
+  query: { userId }, // để backend nhận socket.handshake.query.userId
 });
