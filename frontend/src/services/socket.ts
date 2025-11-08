ec68e972706cc00445ff10c85ce89a1847b87a26
Lê Thành Phú <lephu240905@gmail.com>
2025-11-08 08:43:02 +0700

    final

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -3,6 +3,9 @@ import { io } from "socket.io-client";
 // Lấy userId từ localStorage hoặc cookie
 const userId = localStorage.getItem("userId");
 
+// ✅ Dùng chung origin với backend
+// Khi chạy dev: http://localhost:5001
+// Khi build và deploy: https://webchat-533n.onrender.com
 const BASE_URL =
   import.meta.env.MODE === "production"
     ? window.location.origin
@@ -10,6 +13,6 @@ const BASE_URL =
 
 export const socket = io(BASE_URL, {
   withCredentials: true,
-  transports: ["websocket"],
-  query: { userId }, // để backend nhận socket.handshake.query.userId
+  transports: ["websocket"], // Ưu tiên websocket
+  query: { userId }, // Gửi userId cho backend
 });
